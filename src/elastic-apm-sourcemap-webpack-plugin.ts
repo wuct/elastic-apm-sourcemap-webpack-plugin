969bf99336612bf9a8a143dfcc3ef0709b4eb519
ct <wu.chingting@gmail.com>
2019-02-19 02:16:06 +0800

    fix(lint and add tests):

--- src/elastic-apm-sourcemap-webpack-plugin.ts
+++ src/elastic-apm-sourcemap-webpack-plugin.ts
@@ -1,55 +1,55 @@
-import * as R from 'ramda'
-import fetch from 'node-fetch'
-import FormData from 'form-data'
+import * as R from 'ramda';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 
 // TODO: fix type errors
 
 type config = {
-  serviceName: string
-  serviceVersion: string
-  publicPath: string
-  serverURL: string
-}
+  serviceName: string;
+  serviceVersion: string;
+  publicPath: string;
+  serverURL: string;
+};
 
 export default class ElasticAPMSourceMapPlugin {
-  config: config
+  config: config;
   constructor(config: config) {
-    this.config = config
+    this.config = config;
   }
   apply(compiler) {
     compiler.hooks.afterEmit.tapPromise('ElasticAPMSourceMapPlugin', compilation => {
-      const { chunks } = compilation.getStats().toJson()
+      const { chunks } = compilation.getStats().toJson();
 
       return R.compose(
         promises => Promise.all(promises),
         R.map(({ sourceFile, sourceMap }) => {
-          const formData = new FormData()
-          formData.append('sourcemap', compilation.assets[sourceMap].source())
-          formData.append('service_version', this.config.serviceVersion)
-          formData.append('bundle_filepath', `${this.config.publicPath}/${sourceFile}`)
-          formData.append('service_name', this.config.serviceName)
+          const formData = new FormData();
+          formData.append('sourcemap', compilation.assets[sourceMap].source());
+          formData.append('service_version', this.config.serviceVersion);
+          formData.append('bundle_filepath', `${this.config.publicPath}/${sourceFile}`);
+          formData.append('service_name', this.config.serviceName);
 
           return fetch(this.config.serverURL, {
             method: 'POST',
             body: formData
           })
             .then(response => {
-              if (response.ok) return response.json()
+              if (response.ok) return response.json();
               else {
-                throw new Error(`Error while uploading ${sourceMap} to Elastic APM`)
+                throw new Error(`Error while uploading ${sourceMap} to Elastic APM`);
               }
             })
             .then(() => {
-              console.info(`Uploaded ${sourceMap} to Elastic APM`) // eslint-disable-line no-console
-            })
+              console.info(`Uploaded ${sourceMap} to Elastic APM`); // eslint-disable-line no-console
+            });
         }),
         R.map(({ files }) => {
-          const sourceFile = R.find(file => /\.js$/.test(file), files)
-          const sourceMap = R.find(file => /\.js\.map$/.test(file), files)
+          const sourceFile = R.find(file => /\.js$/.test(file), files);
+          const sourceMap = R.find(file => /\.js\.map$/.test(file), files);
 
-          return { sourceFile, sourceMap }
+          return { sourceFile, sourceMap };
         })
-      )(chunks)
-    })
+      )(chunks);
+    });
   }
 }
--- test/integration.test.ts
+++ test/integration.test.ts
@@ -1,50 +1,71 @@
-import webpack from 'webpack'
-import path from 'path'
-import mockFetch from 'jest-fetch-mock'
-import fetch from 'node-fetch'
-import ElasticAPMSourceMapPlugin from '../src/elastic-apm-sourcemap-webpack-plugin'
+import webpack from 'webpack';
+import path from 'path';
+import mockFetch from 'jest-fetch-mock';
+import fetch from 'node-fetch';
+import ElasticAPMSourceMapPlugin from '../src/elastic-apm-sourcemap-webpack-plugin';
 
-jest.mock('node-fetch', () => mockFetch)
+jest.mock('node-fetch', () => mockFetch);
+
+const config = {
+  entry: path.resolve(__dirname, './entry.js'),
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new ElasticAPMSourceMapPlugin({
+      serviceName: 'mock-service',
+      serviceVersion: 'mock-version',
+      publicPath: '/mock-folder',
+      serverURL: 'mock-url'
+    })
+  ]
+};
 
 beforeEach(() => {
-  fetch.resetMocks()
-})
+  fetch.resetMocks();
+});
 
 test('ok', cb => {
-  fetch.mockResponse(JSON.stringify('ok'))
-
-  const config = {
-    entry: path.resolve(__dirname, './entry.js'),
-    mode: 'production',
-    devtool: 'source-map',
-    plugins: [
-      new ElasticAPMSourceMapPlugin({
-        serviceName: 'mock-service',
-        serviceVersion: 'mock-version',
-        publicPath: '/mock-folder',
-        serverURL: 'mock-url'
-      })
-    ]
-  }
+  fetch.mockResponse(JSON.stringify('ok'));
 
   webpack(config, (err, stats) => {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
 
     if (stats.hasErrors()) {
-      return cb(stats.toJson().errors)
+      return cb(stats.toJson().errors);
     }
 
-    expect(fetch.mock.calls.length).toEqual(1)
-    console.log('calls', fetch.mock.calls)
-    expect(fetch.mock.calls[0][0]).toEqual('mock-url')
-    expect(fetch.mock.calls[0][1].method).toEqual('POST')
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(fetch.mock.calls[0][0]).toEqual('mock-url');
+    expect(fetch.mock.calls[0][1].method).toEqual('POST');
 
     // TODO: check body
 
-    cb()
-  })
-})
+    cb();
+  });
+});
 
-// TODO: test error handling
+test('failed', cb => {
+  fetch.mockReject('failed');
+
+  webpack(config, err => {
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(err).toEqual('failed');
+    setTimeout(() => {
+      cb();
+    }, 100);
+  });
+});
+
+test('400', cb => {
+  fetch.mockResponses(['failed', { status: 400 }]);
+
+  webpack(config, err => {
+    expect(fetch.mock.calls.length).toEqual(1);
+    expect(err).toBeInstanceOf(Error);
+    setTimeout(() => {
+      cb();
+    }, 100);
+  });
+});
